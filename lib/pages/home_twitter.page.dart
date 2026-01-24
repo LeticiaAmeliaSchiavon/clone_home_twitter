48b149c659d284cb4e65e0498b9bab6ebb324f48
Leticia Schiavon <leticiaschiavon20@gmail.com>
2026-01-23 20:10:46 -0400

    project completed

---
+++ lib/pages/home_twitter.page.dart
@@ -0,0 +1,72 @@
+import 'package:clone_home_twitter/widgets/custom_button.widget.dart';
+import 'package:clone_home_twitter/widgets/social_button.widget.dart';
+import 'package:clone_home_twitter/widgets/terms_and_privacy.widget.dart';
+import 'package:flutter/material.dart';
+
+class HomeTwiiterPage extends StatelessWidget {
+  const HomeTwiiterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 35),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset("assets/images/twitter.png", width: 30, height: 30),
+              const Expanded(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "See what`s happening in the world right now.",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SocialButton(
+                pathImage: "assets/images/google.png",
+                text: "Continue with Google",
+              ),
+              const SizedBox(height: 15),
+              const SocialButton(
+                pathImage: "assets/images/apple.png",
+                text: "Continue with Apple",
+              ),
+              const SizedBox(height: 15),
+              Row(
+                children: [
+                  Expanded(child: Divider(color: Colors.black)),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text("or"),
+                  ),
+                  Expanded(child: Divider(color: Colors.black)),
+                ],
+              ),
+              const SizedBox(height: 5),
+              const CustomButton(text: "Create account"),
+              const SizedBox(height: 30),
+              const TermsAndPrivacy(),
+              const SizedBox(height: 50),
+              RichText(
+                text: TextSpan(
+                  text: "Have an acoount already? ",
+                  style: TextStyle(color: Colors.black),
+                  children: [
+                    TextSpan(
+                      text: "Log in",
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/terms_and_privacy.widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class TermsAndPrivacy extends StatelessWidget {
+  const TermsAndPrivacy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text.rich(
+      TextSpan(
+        text: "By singing up, you agress to our",
+        children: [
+          TextSpan(
+            text: "Terms",
+            style: TextStyle(color: Colors.blue),
+          ),
+          TextSpan(text: ", "),
+          TextSpan(
+            text: "Privacy Policy",
+            style: TextStyle(color: Colors.blue),
+          ),
+          TextSpan(text: ", and"),
+          TextSpan(
+            text: " Cookie Use",
+            style: TextStyle(color: Colors.blue),
+          ),
+          TextSpan(text: "."),
+        ],
+      ),
+    );
+  }
+}
